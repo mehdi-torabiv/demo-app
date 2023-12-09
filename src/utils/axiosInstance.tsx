c4ddf15ee445eceac8e407982fc03bf6646de6c1
zuies <zurabichkhikvadze66@gmail.com>
2023-12-09 12:48:41 +0300

    create simple useQuery for product list

--- src/utils/axiosInstance.tsx
+++ src/utils/axiosInstance.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.BASE_URL_DEMO_APP,
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
 });
 
 axiosInstance.interceptors.request.use(
