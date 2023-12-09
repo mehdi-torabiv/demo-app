68adee3d9926b1d0627c46bc5a10055a2a19114d
zuies <zurabichkhikvadze66@gmail.com>
2023-12-09 11:54:43 +0300

    update tanstackProvider

--- src/providers/TanstackProvider.tsx
+++ src/providers/TanstackProvider.tsx
@@ -1,5 +1,4 @@
 "use client";
-import React, { useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
