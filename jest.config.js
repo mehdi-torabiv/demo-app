10f30ab30616d20fcbcd73f847438112696b1087
zuies <zurabichkhikvadze66@gmail.com>
2023-12-09 12:24:31 +0300

    install testing libraries

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+    dir:'./'
+})
+
+// Add any custom config to be passed to Jest
+/** @type {import('jest').Config} */
+const config = {
+    // Add more setup options before each test is run
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+    testEnvironment:'jest-environment-jsdom',
+    preset:'ts-jest'
+}
+
+// createJestConfig is exported this way to ensure that next/jest can export
+module.exports = createJestConfig(config)
